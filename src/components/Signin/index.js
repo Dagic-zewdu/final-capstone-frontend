f5064d659c5b8d6aac268216172d3047bf844145
Dagi <dagi.zewdu.dz@gmail.com>
2022-07-09 16:24:45 +0300

    Added configuration and also done google login setup

--- src/components/Signin/index.js
+++ src/components/Signin/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import GoogleLogin from 'react-google-login';
+import config from '../../config/config';
 
 function GoogleSignIn() {
+  const signInwithGoogle = (user) => {
+
+  };
+  const signInGoogleFailure = (user) => {
+
+  };
   return (
-    <div>GoogleSignIn</div>
+    <GoogleLogin
+      clientId={config.GoogleClientId}
+      onSuccess={signInwithGoogle}
+      onFailure={signInGoogleFailure}
+      cookiePolicy="single_host_origin"
+
+      render={(renderProps) => (
+        <CustomButton onClick={renderProps.onClick} disabled={renderProps.disabled} isGoogleSignIn>
+          Sign in with Google
+        </CustomButton>
+
+      )}
+    />
   );
 }
 
---
+++ src/config/config.js
@@ -0,0 +1,4 @@
+const config = {
+  GoogleClientId: '1007716163290-164glemanmnstqt2hkn7en251b3ofnud.apps.googleusercontent.com',
+};
+export default config;
