a77f03b4ba34f21a3f5b347ae7c36386452365d1
Dagi <dagi.zewdu.dz@gmail.com>
2022-07-09 16:31:16 +0300

    Added toasts for notification

--- src/components/Signin/index.js
+++ src/components/Signin/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import GoogleLogin from 'react-google-login';
+import { Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 import config from '../../config/config';
 
 function GoogleSignIn() {
   const signInwithGoogle = (user) => {
-
+    console.log(user);
   };
-  const signInGoogleFailure = (user) => {
-
+  const signInGoogleFailure = (err) => {
+    console.log(err);
   };
   return (
     <GoogleLogin
@@ -15,12 +18,12 @@ function GoogleSignIn() {
       onSuccess={signInwithGoogle}
       onFailure={signInGoogleFailure}
       cookiePolicy="single_host_origin"
-
       render={(renderProps) => (
-        <CustomButton onClick={renderProps.onClick} disabled={renderProps.disabled} isGoogleSignIn>
-          Sign in with Google
-        </CustomButton>
-
+        <Button variant="outline-warning" onClick={renderProps.onClick} disabled={renderProps.disabled} isGoogleSignIn>
+          <FontAwesomeIcon icon={faGoogle} className="bg-warning" />
+          {' '}
+          Sign in
+        </Button>
       )}
     />
   );
