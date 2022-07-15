9987fd0777e19384ac710fec49f5082feaced8a3
Dagi <dagi.zewdu.dz@gmail.com>
2022-07-15 13:32:47 +0300

    Added image for the details page

--- src/components/Motorcycle/Details/index.js
+++ src/components/Motorcycle/Details/index.js
@@ -1,29 +1,53 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Card, Carousel } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
 import { fetchMotorCycleAsync } from '../../../Redux/actions/motorcycle';
+import responsive from '../../../utils/responsive';
 import AllContainer from '../../Shared/AllContainer';
 
 function MotorCycleDetails() {
   const { id } = useParams();
   const {
     createdBy, data, loading, error,
-  } = useSelector((state) => state.motorcycle.motorcycle);
+  } = useSelector((state) => state.motorcycle?.motorcycle);
   const dispatch = useDispatch();
   useEffect(() => {
     if (data?.id !== id) {
       dispatch(fetchMotorCycleAsync(id));
     }
   }, [id]);
+  const [image, setImage] = useState(data?.image[0]);
   return (
-    <AllContainer navigation={false}>
+    <AllContainer navigation={false} loading={loading} error={error}>
       <div className="container">
         <div className="row">
           <div className="col-lg-9">
             <Link to="/">
               <h1 className="font-italic logo display-4">Bcycom</h1>
               <div className="mt-3">
-                <img src="" alt="" />
+                <img src={image} alt="" style={{ width: '100%' }} />
+              </div>
+              <div className="mt-3">
+                <div className="row">
+                  <Carousel
+                    className="w-100"
+                    keyBoardControl
+                    itemClass="mr-10"
+                    responsive={responsive([5, 3, 2])}
+                  />
+                  {
+                    data?.images.filter((i) => i !== image)
+                      .map((i) => (
+                        <Card.Img
+                          key={i}
+                          src={i}
+                          className={i === image ? 'bodered' : ''}
+                          onClick={() => setImage(i)}
+                        />
+                      ))
+                  }
+                </div>
               </div>
             </Link>
           </div>
