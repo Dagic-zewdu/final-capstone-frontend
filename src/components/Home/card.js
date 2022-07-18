8396714d62c76718dd99acabc28fbf4897ef2665
Dagi <dagi.zewdu.dz@gmail.com>
2022-07-19 02:37:43 +0300

    Fixed lintor errors

--- src/components/Home/card.js
+++ src/components/Home/card.js
@@ -17,9 +17,16 @@ function HomeCard({ cycle }) {
           style={{ height: '400px', objectFit: 'cover', opacity: 2 }}
         />
       </div>
-      <Card.Title className="text-center mt-3 text-uppercase fa-4x" style={{ color: '#000', fontFamily: "'Passion One', cursive", letterSpacing: '2px', fontSize: '30px' }}>{cycle?.title}</Card.Title>
+      <Card.Title
+        className="text-center mt-3 text-uppercase fa-4x"
+        style={{
+          color: '#000', fontFamily: "'Passion One', cursive", letterSpacing: '2px', fontSize: '30px',
+        }}
+      >
+        {cycle?.title}
+      </Card.Title>
       <hr style={{ borderTop: '0.5px dashed #eee' }} />
-      <p className="text-center mb-5" style={{ fontFamily: "'Rubik', sans-serif", fontSize: '14px',  }}>{cycle?.description.slice(0, 50)}</p>
+      <p className="text-center mb-5" style={{ fontFamily: "'Rubik', sans-serif", fontSize: '14px' }}>{cycle?.description.slice(0, 50)}</p>
 
     </Card>
   );
