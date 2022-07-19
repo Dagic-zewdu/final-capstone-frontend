9d0970cbe3855ba268a0ec942b4e3f5115fcbe8c
oluyaratosin123 <oluyaratosin123@gmail.com>
2022-07-19 02:38:46 -0700

    UI - add home skeleton container

---
+++ src/components/Skeleton/HomeCardSkeleton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomeCycleSkeleton() {
+  return (
+    <div className="container d-flex align-items-center justify-content-center w-100" style={{ minHeight: '100vh' }}>
+
+    </div>
+  );
+}
+
+export default HomeCycleSkeleton;
