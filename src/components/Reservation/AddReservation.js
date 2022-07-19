8d74d66e86acef1f605976fa5b8489688f0620ad
oluyaratosin123 <oluyaratosin123@gmail.com>
2022-07-19 00:50:03 -0700

    UI - add reservation form style

--- src/components/Reservation/AddReservation.js
+++ src/components/Reservation/AddReservation.js
@@ -45,7 +45,7 @@ function AddReservation({
   return (
     <div id="myNav" className="overlay" style={{ height: show ? '100%' : '0%' }}>
       <Button
-        className="closebtn mt-4 fs-4 border-0 text-light fw-bolder rounded bg-danger"
+        className="closebtn mt-4 fs-4 border-0 text-light fw-bolder rounded-circle bg-danger"
         variant="outline-primary"
         style={{ fontFamily: "'Rubik', sans-serif" }}
         onClick={() => showReservation(false)}
@@ -53,23 +53,24 @@ function AddReservation({
         X
       </Button>
       <div className="overlay-content">
-        <h5>{cycle?.title}</h5>
-        <h4 className="mt-3">{cycle?.description}</h4>
+        <h5 className='title fw-bolder mt-2'>{cycle?.title}</h5>
+        <h4 className="mt-3 desc fw-bolder mt-2">{cycle?.description}</h4>
         <form onSubmit={handleSubmit}>
-          <div className="d-flex align-items-center mb-3 mt-3 justify-content-around w-100">
+          <div className="d-flex align-items-center mb-3 fixed-bottom container mt-5 justify-content-between w-100">
             <FloatingLabel
               controlId="floatingInput"
               label="Drop your phone"
-              className="w-75 mr-10"
+              className="w-75 mr-10 form-label fw-bolder"
+              style={{ fontFamily: "'Rubik', sans-serif" }}
             >
-              <Form.Control type="text" value={state.phone} required={edit} onChange={handleChange} id="phone" placeholder="Phone" />
+              <Form.Control type="text" value={state.phone} class="p-4" required={edit} onChange={handleChange} id="phone" placeholder="Phone" />
             </FloatingLabel>
             <Button
-              variant="outline-success"
+              variant="outline-light"
               type="submit"
-              className="w-25"
+              className="w-25 btn-reserve fw-bolder border-2 p-1"
             >
-              {!edit ? 'Reserve' : 'Update'}
+              {!edit ? '+ Reserve' : '+ Update'}
             </Button>
           </div>
 
